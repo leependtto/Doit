a10abe606933d544cafccf73d14bed0cce38e960
Lee Pendexter <lee106@comcast.net>
2017-03-16 01:37:56 -0400

    Initial Commit

---
+++ Doit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doit
+//
+//  Created by Lee Pendexter on 3/16/17.
+//  Copyright © 2017 appdog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
